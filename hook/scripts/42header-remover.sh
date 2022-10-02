8efdf824243dc1d4e4cc34c57e70d3ae3db04033
맹주영 <mjy9088@naver.com>
2022-10-02 20:46:43 +0900

    upgrade 42header remover

--- hook/scripts/42header-remover.sh
+++ hook/scripts/42header-remover.sh
@@ -6,7 +6,7 @@ FILE_NAME="fake_file_name (file name is useless too)    "
 # FILE_NAME="$(basename $1)$(printf %45.45s)" # uncomment when norminette starts complain it
 PLACEHOLDER="PLACEHOLDER_FOR_FILE_NAME_UP_TO_45_CHARACTERS"
 
-if head -11 < "$1" | grep -Eo ".{25}$" | cmp -s hook/assets/42header-c-form-11x25.txt; then
+if [ "$( < "$1" tr -d '\n' | grep -Ec \\/\\\*\ \\\*\{74\}\ \\\*\\/\\/\\\*.\*\\\*\\/\\/\\\*.\*\\\*\\/\\/\\\*.\{3\}\(\[\^\ \]\*\).\*\\\*\\/\\/\\\*.\*\\\*\\/\\/\\\*\ \ \ By:\ \(\[\^\ \]\*\).\*\\\*\\/\\/\\\*.\*\\\*\\/\\/\\\*\ \ \ Created:\ \(\[\^\ \]\*\ \[\^\ \]\*\)\ by\ \(\[\^\ \]\*\).\*\\\*\\/\\/\\\*\ \ \ Updated:\ \(\[\^\ \]\*\ \[\^\ \]\*\)\ by\ \(\[\^\ \]\*\).\*\\\*\\/\\/\\\*.\*\\\*\\/\\/\\\*\ \\\*\{74\}\ \\\*\\/)" = "1" ] ; then
   sed "s/$PLACEHOLDER/$FILE_NAME/" < hook/assets/42header-c-template.txt > "$1.tmp"
   tail -n +12 "$1" >> "$1.tmp"
   mv "$1" "$1.bak"
