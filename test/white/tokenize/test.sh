88b27ab054af5dc7c37b2c5343f90a87b943345f
맹주영 <mjy9088@naver.com>
2022-10-08 22:32:47 +0900

    move common files

--- test/white/tokenize/test.sh
+++ test/white/tokenize/test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-find data -name "*.json" | sort | sed "s/.json//" | while IFS= read -r line
+find ../data -name "*.json" | sort | sed "s/.json//" | while IFS= read -r line
 do
   $1 "$line.json" | cmp -s "$line.txt"
 done
